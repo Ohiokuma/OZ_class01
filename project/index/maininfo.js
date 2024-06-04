a8ed405cee7edfef1f3e1dcc9fb81610f87ba17f
Ohiokuma <153696019+Ohiokuma@users.noreply.github.com>
2024-06-04 15:01:38 +0900

    Add files via upload

---
+++ project/index/maininfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainInfo.css';
+
+const MainInfo = () => {
+    return (
+        <main className="main-info">
+            <h2>우리 동네에서 찾는 더덕알바</h2>
+            <p>걸어서 10분 거리의 동네 알바를 여기 다 있어요.</p>
+        </main>
+    );
+}
+
+export default MainInfo;
